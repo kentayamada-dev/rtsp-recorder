364ebbac2bad38dc8cfea09b150ef0b97f55a63e
Kenta Yamada <kentayamada058@gmail.com>
2025-12-17 01:50:47 -0500

    add menu

--- src/main/menu.ts
+++ src/main/menu.ts
@@ -1,6 +1,39 @@
-import { Menu } from "electron";
+import { Menu, shell, type MenuItemConstructorOptions } from "electron";
 
 export const createMenu = ({ isDev }: { isDev: boolean }) => {
-  if (isDev) return;
-  Menu.setApplicationMenu(null);
+  const fileMenu: MenuItemConstructorOptions = {
+    label: "File",
+    submenu: [{ role: "quit" }],
+  };
+
+  const devMenu: MenuItemConstructorOptions = {
+    label: "View",
+    submenu: [{ role: "forceReload" }, { role: "toggleDevTools" }],
+  };
+
+  const aboutMenu: MenuItemConstructorOptions = {
+    label: "About",
+    submenu: [
+      {
+        label: "Developer Info",
+        click: () => {
+          shell.openExternal("https://github.com/kentayamada-dev");
+        },
+      },
+      {
+        label: "App Source Code",
+        click: () => {
+          shell.openExternal(
+            "https://github.com/kentayamada-dev/rtsp-recorder",
+          );
+        },
+      },
+    ],
+  };
+
+  const template: MenuItemConstructorOptions[] = isDev
+    ? [fileMenu, devMenu]
+    : [fileMenu, aboutMenu];
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 };
